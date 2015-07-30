69844eb9f9534ef23f6bd1c6c93cc97f19682157
IuliiaKot <kotlenko.julia@gmail.com>
2015-07-30 02:07:18 -0400

    send via mailgun

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails', '4.2.1'
 # Use sqlite3 as the database for Active Record
 
 gem 'httparty'
-
+gem 'mailgun-ruby', '~>1.0.2'
+gem 'rest-client'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/send_emails_controller.rb
+++ app/controllers/send_emails_controller.rb
@@ -1,3 +1,7 @@
+require 'mailgun'
+require "net/https"
+require "uri"
+require 'rest-client'
 class SendEmailsController < ApplicationController
 
   def index
@@ -12,17 +16,51 @@ class SendEmailsController < ApplicationController
       @body = params[:g][:body]
       #SendMail.welcome(@from, @to, @subject, @body).deliver_now
       # require "httparty"
-      url = "https://sendgrid.com/api/mail.send.json"
+      #  url = "https://sendgrid.com/api/mail.send.json"
+      # #
+      # response = HTTParty.post url, :body => {
+      #   "api_user" => ENV['SENDGRID_USERNAME'],
+      #   "api_key" =>  ENV['SENDGRID_PASSWORD'],
+      #   "to" => @to,
+      #   "from" => @from,
+      #   "subject" => @subject,
+      #   "text" => @body
+      # }
+      # if response["message"] != 'success'
+        #  "api_user" => "key-6c33972ee3f7400fcde10f3b24fbcba5",
 
-      response = HTTParty.post url, :body => {
-        "api_user" => ENV['SENDGRID_USERNAME'],
-        "api_key" =>  ENV['SENDGRID_PASSWORD'],
-        "to" => @to,
-        "from" => @from,
-        "subject" => @subject,
-        "text" => @body
-      }
-      response.body
+              # mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+              # message_params = {:from    => @from,
+              #                   :to      => @to,
+              #                   :subject => 'Sample Mail using Mailgun API',
+              #                   :text    => 'This mail is sent using Mailgun API via mailgun-ruby'}
+              # mg_client.send_message ENV['domain'], message_params
+
+        # uri = URI.parse("https://api.mailgun.net/v3/sandbox4bd8677766bb465eb60b0bb14596b98c.mailgun.org/messages")
+        #
+        # data = {'to'        => @to,
+        #       'from'      => @from,
+        #       'subject'   => @subject,
+        #       'text'      => @body}
+        #
+        # http = Net::HTTP.new(uri.host, uri.port)
+        # http.use_ssl = true
+        #
+        # request = Net::HTTP::Post.new(uri.request_uri)
+        # request.basic_auth("api", ENV['MAILGUN_API_KEY'])
+        # request.set_form_data(data)
+        # response = http.request(request)
+
+        api = "https://api:#{ENV['MAILGUN_API_KEY']}@api.mailgun.net/v2/sandbox4bd8677766bb465eb60b0bb14596b98c.mailgun.org"
+
+        RestClient.post api+"/messages",
+          :from => @from,
+          :to => @to,
+          :subject => "This is subject",
+          :text => "Text body",
+          :html => "<b>HTML</b> version of the body!"
+      # response.body
+    #end
     end
   end
 
